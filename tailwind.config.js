245fbb7b0af71428f333073f8d8ea5fbb905854c
YOUSUF4949 <yousufalibangladesh49@gmail.com>
2024-08-21 13:03:54 -0700

    B-10-A3 All setup done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          manrope: ['Manrope', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
